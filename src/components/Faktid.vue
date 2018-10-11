6f16ce272ae7e6a523fdef0a162a24e4c1b129c9
Riina Häidkind <riina.haidkind@khk.ee>
2018-10-11 15:33:27 +0300

    Lisasin inkremendi ja korrastasin vastuse paneeli.

--- src/components/Faktid.vue
+++ src/components/Faktid.vue
@@ -3,10 +3,16 @@
   <h1>Numbrifaktid</h1>
   <p>Huvitavad faktid iga numbri kohta</p>
   <input type="number" min="0" v-model="number">
-  <p>{{data}}</p>
+  <input type="inkrement" min="0" v-model="inkrement">
+    <p v-for="(item, index) in data" :key="index">{{ item }}</p>
+    
   </div>
 </template>
 
+   // vaja saada sarnane URL
+    // http://numbersapi.com/1..3
+    
+    
 <script>
     const axios = require('axios')
     
@@ -14,25 +20,37 @@ export default {
   name: 'Faktid',
   data (){
       return {
-    data: '',
-    number: 0
+        data: 0,
+        number: 0,
+        inkrement: 0,
+        }
+    },
+    methods: {
+        getResponse () {
+            if (this.number !== '' && this.inkrement !== '') { 
+                let baseUrl = 'http://numbersapi.com/'
+                let url = `${baseUrl}${this.number}..${parseInt(this.number) + parseInt(this.inkrement)}`;
+                
+                axios.get(url)
+                .then(response => {
+                    this.data = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            }
         }
     },
     watch: {
         number() {
-            if (this.number !== '') { 
-        let url = `http://numbersapi.com/${this.number}`
-        
-        axios.get(url)
-            .then(response => {
-            this.data = response.data
-        })
-            .catch(error => {
-                console.log(error)
-            })
+            this.getResponse()
+        },
+        inkrement () {
+            this.getResponse()
+            
         }
+            
     }
-}
 }
 </script>
 
